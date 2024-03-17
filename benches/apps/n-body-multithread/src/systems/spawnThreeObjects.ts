cbd56ab5e721043129dc849a15e869479606941d
Nate <mrtn.nathaniel@gmail.com>
2024-03-17 13:00:56 -0700

    🐛 Fixed instanced mesh scaling

--- benches/apps/n-body-multithread/src/systems/spawnThreeObjects.ts
+++ benches/apps/n-body-multithread/src/systems/spawnThreeObjects.ts
@@ -9,8 +9,6 @@ import { ThreeObject } from '../components/ThreeObject';
 const bodyQuery = defineQuery([Position, Velocity, Mass, Circle]);
 const enterBodyQuery = enterQuery(bodyQuery);
 
-const normalize = (x: number, min: number, max: number) => (x - min) / (max - min);
-
 export const spawnThreeObjects = defineSystem((world) => {
     const eids = enterBodyQuery(world);
 
@@ -20,27 +18,7 @@ export const spawnThreeObjects = defineSystem((world) => {
 		const geometry = new THREE.SphereGeometry(Circle.radius[0], 12, 12);
 		const material = new THREE.MeshBasicMaterial({ color: new THREE.Color().setRGB(1,1,1) });
 		const instancedMesh = new THREE.InstancedMesh(geometry, material, maxInstances);
-		const dummy = new THREE.Object3D();
-	
-		for (let i = 0; i < eids.length; i++) {
-			const eid = eids[i];
-	
-			dummy.position.set(
-				Position.x[eid],
-				Position.y[eid],
-				0,
-			);
 
-			const r = normalize(Circle.radius[eid], 0, 100)
-			dummy.scale.set(r,r,r);
-
-			dummy.updateMatrix();
-
-			instancedMesh.setMatrixAt(eid, dummy.matrix);
-	
-		}
-		instancedMesh.instanceMatrix.needsUpdate = true;
-		instancedMesh.computeBoundingSphere();
 		scene.add(instancedMesh);
 		
 		ThreeObject[0] = instancedMesh;
--- benches/apps/n-body-multithread/src/systems/syncThreeObjects.ts
+++ benches/apps/n-body-multithread/src/systems/syncThreeObjects.ts
@@ -1,20 +1,27 @@
 import { defineSystem } from '@bitecs/classic';
-import { Color, Position, bodyQuery } from '@sim/n-body-multithread';
+import { Circle, Color, Position, bodyQuery } from '@sim/n-body-multithread';
 import { ThreeObject } from '../components/ThreeObject';
 import * as THREE from 'three'
 
 const normalize = (x: number, min: number, max: number) => (x - min) / (max - min);
 
+const dummy = new THREE.Object3D();
+const color = new THREE.Color();
+
 export const syncThreeObjects = defineSystem((world) => {
 	const eids = bodyQuery(world);
 	const instancedMesh = ThreeObject[0]
-	const matrix = new THREE.Matrix4();
-	const color = new THREE.Color();
 
     for (let i = 0; i < eids.length; i++) {
         const eid = eids[i];
-        matrix.setPosition(Position.x[eid], Position.y[eid], 0);
-        instancedMesh.setMatrixAt(eid, matrix);
+        dummy.position.set(Position.x[eid], Position.y[eid], 0);
+
+		const radius = normalize(Circle.radius[eid], 0, 60)
+		dummy.scale.set(radius,radius,radius);
+
+		dummy.updateMatrix();
+
+        instancedMesh.setMatrixAt(eid, dummy.matrix);
 
 		const r = normalize(Color.r[eid], 0, 255);
 		const g = normalize(Color.g[eid], 0, 255);
