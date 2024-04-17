021019996ec4329f35ac914b9375ff8f3e415e5a
Nate <mrtn.nathaniel@gmail.com>
2024-04-17 08:33:58 -0700

    ✨ classic: added cleaner version of relations

--- packages/classic/src/relation/Relation.ts
+++ packages/classic/src/relation/Relation.ts
@@ -1,5 +1,6 @@
 import { Schema } from "..";
 import { ComponentType, defineComponent } from "..";
+import { $schema } from "../component/symbols";
 
 const $pairsMap = Symbol('pairsMap')
 export const $isPairComponent = Symbol('isPairComponent')
@@ -9,20 +10,31 @@ export const Wildcard = '*'
 
 export type RelationType<T extends Schema> = T & {
     [$pairsMap]: Map<number | string, ComponentType<T>>
-}
+    [$schema]: Schema
+} & ((target: string | number) => ComponentType<T>)
 
 export const defineRelation = 
     <T extends Schema>(schema: T = {} as T): RelationType<T> => {
-        const relation = schema as RelationType<T>
-        Object.defineProperty(relation, $pairsMap, { enumerable: false, writable: true, value: new Map() })
-        return relation
+        const pairsMap = new Map();
+        const relation = function (target: number | string) {
+            if (!pairsMap.has(target)) {
+                const component = defineComponent(schema) as any
+                component[$isPairComponent] = true;
+                component[$relation] = relation;
+                pairsMap.set(target, component);
+            }
+            return pairsMap.get(target) as ComponentType<T>;
+        }
+        Object.defineProperty(relation, $pairsMap, { enumerable: false, writable: true, value: pairsMap })
+        Object.defineProperty(relation, $schema, { enumerable: false, writable: true, value: schema })
+        return relation as RelationType<T>
     }
 
-export const Pair = <T extends Schema>(relation: T, target: number | string): ComponentType<T> => {
+export const Pair = <T extends Schema>(relation: RelationType<T>, target: number | string): ComponentType<T> => {
     const map = (relation as RelationType<T>)[$pairsMap]
 
     if (!map.has(target)) {
-        const component = defineComponent(relation) as any
+        const component = defineComponent(relation[$schema]) as any
         component[$isPairComponent] = true;
         component[$relation] = relation;
         map.set(target, component);
