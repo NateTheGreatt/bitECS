81ec991ff9649bd4c45edddfcbba596e6db853a5
Weston Vincze <westonvincze@gmail.com>
2024-07-01 19:41:01 -0400

    PR feedback

--- packages/classic/src/component/Component.ts
+++ packages/classic/src/component/Component.ts
@@ -183,7 +183,7 @@ export const addComponent = (world: World, component: Component, eid: number, re
 		// if it's an exclusive relation, remove the old target
 		if (relation[$exclusiveRelation] === true && target !== Wildcard) {
 			const oldTarget = getRelationTargets(world, relation, eid)[0];
-			if (oldTarget >= 0 && oldTarget !== target) {
+			if (oldTarget !== undefined && oldTarget !== null && oldTarget !== target) {
 				removeComponent(world, relation(oldTarget), eid);
 			}
 		}
