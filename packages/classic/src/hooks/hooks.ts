dc9e1c8fe4587daea3d5defc9787ca98a8fbf6c7
pietrovismara <pp.vismara@gmail.com>
2024-07-20 19:21:40 +0200

    🏷️ core: make world arg generic

--- packages/classic/src/hooks/hooks.ts
+++ packages/classic/src/hooks/hooks.ts
@@ -5,38 +5,41 @@ import { RelationType } from '../relation/types';
 import { World } from '../world/types';
 import { $onAdd, $onRemove } from './symbols';
 
-export function onAdd<Params>(
+export function onAdd<Params, W extends World = World>(
 	prefab: PrefabNode<Params>,
-	cb: (world: World, eid: number, params: Params) => void
+	cb: (world: W, eid: number, params: Params) => void
 ): void;
-export function onAdd<Store, Params>(
+export function onAdd<Store, Params, W extends World = World>(
 	component: Component<Store, Params>,
-	cb: (world: World, store: Store, eid: number, params: Params) => void
+	cb: (world: W, store: Store, eid: number, params: Params) => void
 ): void;
-export function onAdd<Store, Params>(
+export function onAdd<Store, Params, W extends World = World>(
 	relation: RelationType<Component<Store, Params>>,
-	cb: (world: World, store: Store, eid: number, params: Params) => void
+	cb: (world: W, store: Store, eid: number, params: Params) => void
 ): void;
 export function onAdd(subject: any, cb: any) {
 	subject[$onAdd] = cb;
 }
 
-export function onRemove<Params>(
+export function onRemove<Params, W extends World = World>(
 	prefab: PrefabNode<Params>,
-	cb: (world: World, eid: number) => void
+	cb: (world: W, eid: number) => void
 ): void;
-export function onRemove<Store, Params>(
+export function onRemove<Store, Params, W extends World = World>(
 	component: Component<Store, Params>,
-	cb: (world: World, store: Store, eid: number, reset: boolean) => void
+	cb: (world: W, store: Store, eid: number, reset: boolean) => void
 ): void;
-export function onRemove<Store, Params>(
+export function onRemove<Store, Params, W extends World = World>(
 	relation: RelationType<Component<Store, Params>>,
-	cb: (world: World, store: Store, eid: number, reset: boolean) => void
+	cb: (world: W, store: Store, eid: number, reset: boolean) => void
 ): void;
 export function onRemove(subject: any, cb: any) {
 	subject[$onRemove] = cb;
 }
 
-export function onTargetRemoved(subject: RelationType<any>, cb: (world: World, eid: number) => void) {
+export function onTargetRemoved<W extends World = World>(
+	subject: RelationType<any>,
+	cb: (world: W, eid: number) => void
+) {
 	subject[$onTargetRemoved] = cb;
 }
