5e2ce7a37950771ba9e6995367dbc78760cc3da1
Nate <mrtn.nathaniel@gmail.com>
2024-09-08 14:59:18 -0700

    legacy fixes + build

--- dist/legacy/index.d.ts
+++ dist/legacy/index.d.ts
@@ -1,40 +1,47 @@
-import { ComponentRef, QueryTerm, World, EntityId } from '../core';
+import { ComponentRef, QueryTerm, EntityId } from '../core';
+export interface IWorld {
+}
 export declare const defineQuery: (terms: QueryTerm[]) => {
-    (world: World): readonly number[];
+    (world: IWorld): readonly number[];
     terms: any[];
 };
-export declare const enterQuery: (queryFn: ReturnType<typeof defineQuery>) => (world: World) => number[];
-export declare const exitQuery: (queryFn: ReturnType<typeof defineQuery>) => (world: World) => number[];
-export declare const addComponent: (world: World, component: ComponentRef, eid: EntityId) => void;
-export declare const hasComponent: (world: World, component: ComponentRef, eid: EntityId) => boolean;
-export declare const removeComponent: (world: World, component: ComponentRef, eid: EntityId) => void;
-export type Schema = {
-    [key: string]: Schema | PrimitiveType | [PrimitiveType, number];
-};
-export type PrimitiveType = 'f32' | 'i32' | 'u32' | 'f64' | 'i8' | 'u8' | 'i16' | 'u16';
+export declare const enterQuery: (queryFn: ReturnType<typeof defineQuery>) => (world: IWorld) => number[];
+export declare const exitQuery: (queryFn: ReturnType<typeof defineQuery>) => (world: IWorld) => number[];
+export declare const addComponent: (world: IWorld, component: ComponentRef, eid: EntityId) => void;
+export declare const hasComponent: (world: IWorld, component: ComponentRef, eid: EntityId) => boolean;
+export declare const removeComponent: (world: IWorld, component: ComponentRef, eid: EntityId) => void;
+export interface ISchema {
+    [key: string]: Type | ListType | ISchema;
+}
+export type Type = 'i8' | 'ui8' | 'ui8c' | 'i16' | 'ui16' | 'i32' | 'ui32' | 'f32' | 'f64' | 'eid';
+export type ListType = readonly [Type, number];
 export declare const Types: {
-    f32: PrimitiveType;
-    i32: PrimitiveType;
-    u32: PrimitiveType;
-    f64: PrimitiveType;
-    i8: PrimitiveType;
-    u8: PrimitiveType;
-    i16: PrimitiveType;
-    u16: PrimitiveType;
+    i8: Type;
+    ui8: Type;
+    ui8c: Type;
+    i16: Type;
+    ui16: Type;
+    i32: Type;
+    ui32: Type;
+    f32: Type;
+    f64: Type;
+    eid: Type;
 };
-type ArrayByType = {
-    'f32': Float32Array;
-    'i32': Int32Array;
-    'u32': Uint32Array;
-    'f64': Float64Array;
+export type TypedArray = Uint8Array | Int8Array | Uint8ClampedArray | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array;
+export type ArrayByType = {
     'i8': Int8Array;
-    'u8': Uint8Array;
+    'ui8': Uint8Array;
+    'ui8c': Uint8ClampedArray;
     'i16': Int16Array;
-    'u16': Uint16Array;
+    'ui16': Uint16Array;
+    'i32': Int32Array;
+    'ui32': Uint32Array;
+    'f32': Float32Array;
+    'f64': Float64Array;
+    'eid': Uint32Array;
 };
-export type ComponentType<T extends Schema> = {
-    [key in keyof T]: T[key] extends PrimitiveType ? ArrayByType[T[key]] : T[key] extends [infer RT, number] ? RT extends PrimitiveType ? ArrayByType[RT][] : unknown : T[key] extends Schema ? ComponentType<T[key]> : unknown;
+export type ComponentType<T extends ISchema> = {
+    [key in keyof T]: T[key] extends Type ? ArrayByType[T[key]] : T[key] extends [infer RT, number] ? RT extends Type ? Array<ArrayByType[RT]> : unknown : T[key] extends ISchema ? ComponentType<T[key]> : unknown;
 };
-export declare const defineComponent: <T extends Schema>(schema: T, max?: number) => ComponentType<T>;
-export {};
+export declare const defineComponent: <T extends ISchema>(schema: T, max?: number) => ComponentType<T>;
 //# sourceMappingURL=index.d.ts.map
--- src/legacy/index.ts
+++ src/legacy/index.ts
@@ -1,20 +1,22 @@
-import { ComponentRef, QueryTerm, World, observe, onAdd, onRemove, query, EntityId } from '../core'
-import { 
+import { ComponentRef, QueryTerm, observe, onAdd, onRemove, query, EntityId } from '../core'
+import {
   addComponent as ecsAddComponent,
-  hasComponent as ecsHasComponent, 
-  removeComponent as ecsRemoveComponent 
+  hasComponent as ecsHasComponent,
+  removeComponent as ecsRemoveComponent
 } from '../core'
 
+export interface IWorld { }
+
 export const defineQuery = (terms: QueryTerm[]) => {
-  const queryFn = (world: World) => query(world, terms)
+  const queryFn = (world: IWorld) => query(world, terms)
   queryFn.terms = terms
   return queryFn
 }
 
 export const enterQuery = (queryFn: ReturnType<typeof defineQuery>) => {
   let queue: number[] = []
-  const initSet = new WeakSet<World>()
-  return (world: World) => {
+  const initSet = new WeakSet<IWorld>()
+  return (world: IWorld) => {
     if (!initSet.has(world)) {
       observe(world, onAdd(...queryFn.terms), (eid: EntityId) => queue.push(eid))
       initSet.add(world)
@@ -27,8 +29,8 @@ export const enterQuery = (queryFn: ReturnType<typeof defineQuery>) => {
 
 export const exitQuery = (queryFn: ReturnType<typeof defineQuery>) => {
   let queue: number[] = []
-  const initSet = new WeakSet<World>()
-  return (world: World) => {
+  const initSet = new WeakSet<IWorld>()
+  return (world: IWorld) => {
     if (!initSet.has(world)) {
       observe(world, onRemove(...queryFn.terms), (eid: EntityId) => queue.push(eid))
       initSet.add(world)
@@ -39,76 +41,109 @@ export const exitQuery = (queryFn: ReturnType<typeof defineQuery>) => {
   }
 }
 
-export const addComponent = (world: World, component: ComponentRef, eid: EntityId) =>
+export const addComponent = (world: IWorld, component: ComponentRef, eid: EntityId) =>
   ecsAddComponent(world, eid, component)
 
-export const hasComponent = (world: World, component: ComponentRef, eid: EntityId) =>
+export const hasComponent = (world: IWorld, component: ComponentRef, eid: EntityId) =>
   ecsHasComponent(world, eid, component)
 
-export const removeComponent = (world: World, component: ComponentRef, eid: EntityId) =>
+export const removeComponent = (world: IWorld, component: ComponentRef, eid: EntityId) =>
   ecsRemoveComponent(world, eid, component)
 
-export type Schema = { [key: string]: Schema | PrimitiveType | [PrimitiveType, number] }
+export interface ISchema {
+  [key: string]: Type | ListType | ISchema
+}
 
-export type PrimitiveType = 'f32' | 'i32' | 'u32' | 'f64' | 'i8' | 'u8' | 'i16' | 'u16';
+export type Type =
+  | 'i8'
+  | 'ui8'
+  | 'ui8c'
+  | 'i16'
+  | 'ui16'
+  | 'i32'
+  | 'ui32'
+  | 'f32'
+  | 'f64'
+  | 'eid'
+
+export type ListType = readonly [Type, number];
 
 export const Types = {
-  f32: 'f32' as PrimitiveType,
-  i32: 'i32' as PrimitiveType,
-  u32: 'u32' as PrimitiveType,
-  f64: 'f64' as PrimitiveType,
-  i8: 'i8' as PrimitiveType,
-  u8: 'u8' as PrimitiveType,
-  i16: 'i16' as PrimitiveType,
-  u16: 'u16' as PrimitiveType
+  i8: 'i8' as Type,
+  ui8: 'ui8' as Type,
+  ui8c: 'ui8c' as Type,
+  i16: 'i16' as Type,
+  ui16: 'ui16' as Type,
+  i32: 'i32' as Type,
+  ui32: 'ui32' as Type,
+  f32: 'f32' as Type,
+  f64: 'f64' as Type,
+  eid: 'eid' as Type
 };
 
-type ArrayByType = {
-  'f32': Float32Array,
-  'i32': Int32Array,
-  'u32': Uint32Array,
-  'f64': Float64Array,
+export type TypedArray =
+  | Uint8Array
+  | Int8Array
+  | Uint8ClampedArray
+  | Int16Array
+  | Uint16Array
+  | Int32Array
+  | Uint32Array
+  | Float32Array
+  | Float64Array
+
+export type ArrayByType = {
+  'i8': Int8Array;
+  'ui8': Uint8Array;
+  'ui8c': Uint8ClampedArray;
+  'i16': Int16Array;
+  'ui16': Uint16Array;
+  'i32': Int32Array;
+  'ui32': Uint32Array;
+  'f32': Float32Array;
+  'f64': Float64Array;
+  'eid': Uint32Array;
+}
+
+// ... existing code ...
+
+const arrayByTypeMap: { [key in Type]: any } = {
   'i8': Int8Array,
-  'u8': Uint8Array,
+  'ui8': Uint8Array,
+  'ui8c': Uint8ClampedArray,
   'i16': Int16Array,
-  'u16': Uint16Array
-};
-
-const arrayByTypeMap: { [key in PrimitiveType]: any } = {
-  'f32': Float32Array,
+  'ui16': Uint16Array,
   'i32': Int32Array,
-  'u32': Uint32Array,
+  'ui32': Uint32Array,
+  'f32': Float32Array,
   'f64': Float64Array,
-  'i8': Int8Array,
-  'u8': Uint8Array,
-  'i16': Int16Array,
-  'u16': Uint16Array
+  'eid': Uint32Array,
 };
 
-export type ComponentType<T extends Schema> = {
+export type ComponentType<T extends ISchema> = {
   [key in keyof T]:
-    T[key] extends PrimitiveType
-    ? ArrayByType[T[key]]
-    : T[key] extends [infer RT, number]
-      ? RT extends PrimitiveType
-        ? ArrayByType[RT][]
-        : unknown
-      : T[key] extends Schema
-        ? ComponentType<T[key]>
-        : unknown;
+  T[key] extends Type
+  ? ArrayByType[T[key]]
+  : T[key] extends [infer RT, number]
+  ? RT extends Type
+  ? Array<ArrayByType[RT]>
+  : unknown
+  : T[key] extends ISchema
+  ? ComponentType<T[key]>
+  : unknown;
 };
 
-export const defineComponent = <T extends Schema>(schema: T, max: number = 1e5): ComponentType<T> => {
-  const createSoA = <U extends Schema>(schema: U, max: number): ComponentType<U> => {
+export const defineComponent = <T extends ISchema>(schema: T, max: number = 1e5): ComponentType<T> => {
+  const createSoA = <U extends ISchema>(schema: U, max: number): ComponentType<U> => {
     const component = {} as ComponentType<U>
     for (const key in schema) {
       if (Array.isArray(schema[key])) {
-        const [type, length] = schema[key] as [PrimitiveType, number]
+        const [type, length] = schema[key] as [Type, number]
         component[key] = Array.from({ length }, () => new arrayByTypeMap[type](max)) as any
       } else if (typeof schema[key] === 'object') {
-        component[key] = createSoA(schema[key] as Schema, max) as any
+        component[key] = createSoA(schema[key] as ISchema, max) as any
       } else {
-        const type = schema[key] as PrimitiveType;
+        const type = schema[key] as Type;
         const TypeConstructor = arrayByTypeMap[type];
         if (TypeConstructor) {
           component[key] = new TypeConstructor(max) as any;
